024ed27bed2a4527c411a44379bf5f9ba862d811
samhelal <93845103+samhelal@users.noreply.github.com>
2021-11-10 03:28:13 +0200

    Add files via upload

---
+++ multiplication.cpp
@@ -0,0 +1,17 @@
+/*Using the statements you wrote in Exercise 2.4, write a
+complete program that calculates and displays the product of
+three integers. Add comments to the code where appropriate.[Note: You’ll need to write the necessary
+using
+declarations or
+directive.] */
+// program show the result of three integars muliplaction 
+#include <iostream>
+using namespace std ;
+int main () { 
+    int x(0) , y(0) , z(0),result(0) ; 
+    cout << " Enter three numbers : " ;
+    cin >> x >> y >> z ;
+    result = x * y * z ;
+    cout << " the resulte of multiplaction is : " << result << endl ;
+
+}
